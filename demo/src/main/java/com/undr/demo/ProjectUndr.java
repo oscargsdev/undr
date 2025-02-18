0a6ed3418f93414a8b52548a45d19a0d028dd183
oscar <oscargs.dev@gmail.com>
2025-02-18 05:45:59 -0600

    Minimal config of Spring Boot project.

---
+++ demo/src/main/java/com/undr/demo/ProjectUndr.java
@@ -0,0 +1,15 @@
+package com.undr.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ProjectUndr {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectUndr.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/undr/demo/ProjectUndrTests.java
@@ -0,0 +1,13 @@
+package com.undr.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectUndrTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
